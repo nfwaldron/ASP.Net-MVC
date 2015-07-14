b21e7ede4866c835c7155d8f56e8fc29dba31038
nfwaldron <waldron.nathan@gmail.com>
2015-07-14 12:36:14 -0700

    Practice with Complex objects and Model View Binding

---
+++ ComplexObjectMVC/ComplexObjectMVC/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComplexObjectMVC.Models
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ ComplexObjectMVC/ComplexObjectMVC/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComplexObjectMVC.Models
+{
+    public class Customer
+    {
+        // Class Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Address ShippingAddress { get; set; }
+        public Address BillingAddress { get; set; }
+
+        // Class Methods
+
+        public Customer()
+        {
+            // In order for the class Customer to have access to the address object,
+            // the constructor must create a new instance of the 'Address' class every
+            // time an object of the customer class is created.
+            this.BillingAddress = new Address();
+            this.ShippingAddress = new Address();
+        }
+
+
+    }
+}
