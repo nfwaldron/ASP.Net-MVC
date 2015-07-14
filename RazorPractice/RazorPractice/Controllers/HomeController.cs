c6d425a99045fa5b9c5ec9de031e8876ece5f02c
nfwaldron <waldron.nathan@gmail.com>
2015-07-14 14:05:59 -0700

    Updated View Model

--- RazorPractice/RazorPractice/Controllers/HomeController.cs
+++ RazorPractice/RazorPractice/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RazorPractice.Models;
+using RazorPractice.Views.Home;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +12,26 @@ namespace RazorPractice.Controllers
     {
         public ActionResult Index()
         {
+            var products = new List<Product>()
+            {
+                new Product{Id = "product1", Name = "Milk", Price = 3.59m, OnSale = true},
+                new Product{Id = "product2", Name = "Bread", Price = 2.49m, OnSale = false},
+                new Product{Id = "product3", Name = "Cheese", Price = 4.09m, OnSale = true},
+            };
+
+            // View Data is essentially a dictionary. This gives me a way to pass objects from the controller to the view.
+            // I can go to my view and insert ViewData["product"] in the 'Home' view.
+            // This is a weakly typed viewing method. NEVER USE VIEWDATA!!!!!!
+            // ViewData["product"] = products;
+
+
+            // IndexVM is view model. 
+            var vm = new IndexVM
+            {
+                // Assign to the Products property of the IndexVM Class the List of Products 
+                Products = products
+            };
+
             return View();
         }
 
---
+++ RazorPractice/RazorPractice/Views/Home/IndexVM.cs
@@ -0,0 +1,16 @@
+﻿using RazorPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RazorPractice.Views.Home
+{
+    public class IndexVM
+    {
+        // When creating view models, just take the name of the view, and stick VM on the end.
+        // Always create view models in the Views section of your project
+            public IList<Product> Products { get; set; }
+        
+    }
+}
