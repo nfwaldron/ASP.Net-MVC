a6415fae1c77a332fc61fc9038262576cd9c0f94
nfwaldron <waldron.nathan@gmail.com>
2015-07-15 07:14:44 -0700

    REdux of Calculator project

---
+++ Calculator/Calculator/Controllers/CalculatorsController.cs
@@ -0,0 +1,104 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorsController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        { 
+            return View();
+        }
+
+        // POST: Calculator
+        // The ActionResult class represents the result of an action method.
+
+        public ActionResult Index(CalculatorForm calculatorVM)
+        {
+            calculatorVM.Result = calculatorVM.Number1 + calculatorVM.Number2;
+
+            // The view object renders a view to the response. 
+            // Return the updated object of the View Model class to the view. 
+            // In the view, the @model reference will allow you to 
+            return View(calculatorVM); 
+        }
+
+
+        // GET: Calculator/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Calculator/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Calculator/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Calculator/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Calculator/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Calculator/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Calculator/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
