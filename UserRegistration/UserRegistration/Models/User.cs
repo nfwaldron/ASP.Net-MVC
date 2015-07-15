e07211456f226f29a5a67d3c42621a27137c7f2e
nfwaldron <waldron.nathan@gmail.com>
2015-07-15 14:14:22 -0700

    User Registration. Practice with form validation

---
+++ UserRegistration/UserRegistration/Models/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserRegistration.Models
+{
+
+    /* Create a User Registration Form that includes the following form fields:
+     * Username - Must be unique in the database.
+     * FirstName – Required
+     * LastName – Required, Must be more than 5 characters
+     * CreditCard – Must be valid credit card number
+     * Password – Required, Must be the same as ConfirmPassword
+     * ConfirmPassword -- Required
+     * Social Security Number – Must be valid social security number format
+     * Comments - Must be less than 50 characters.
+     * Use StackOverflow for regular expressions.
+     */ 
+
+    public class User
+    {
+        // UserName must be unique to the database.
+        // Remote[ Name of action/method you will create in controller, name of the contoller, ErrorMessage, Http Method="Post"
+        [Remote("ValidateUsername", "Users", ErrorMessage="This Username has been taken", HttpMethod="Post" )]
+        [Required(ErrorMessage = "A username is required")]
+        public string UserName { get; set; }
+        
+        //First Name is required
+        [Required(ErrorMessage="First name is required")]
+        public string FirstName { get; set; }
+
+        //Last Name is required
+        [Required(ErrorMessage="First name is required")]
+        public string LastName { get; set; }
+
+        //Credit card number must be valid
+        [Required, CreditCard]
+        public string CreditCard { get; set; }
+
+        // Password required
+        [Required(ErrorMessage="Password required")]
+        public string Password { get; set; }
+
+        // Confirm password required
+        [Required(ErrorMessage = "Confirm password required")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage="SSN required")]
+        public string SocialSecurityNumber { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
