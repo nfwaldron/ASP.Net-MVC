8ca974a306fa9fee066ca962bf7244bca69f1559
nfwaldron <waldron.nathan@gmail.com>
2015-07-21 00:14:30 -0700

    update

--- AlbumApplication/AlbumApplication/Migrations/Configuration.cs
+++ AlbumApplication/AlbumApplication/Migrations/Configuration.cs
@@ -24,19 +24,6 @@ namespace AlbumApplication.Migrations
 
             context.Albums.AddOrUpdate(a => a.Title, albums);
 
-            
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
     }
 }
